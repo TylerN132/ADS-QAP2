0f7a0a3ea0b0f9844e60c93b8c85fbd183b8ba39
Tyler Nolan <tyler.nolan@keyin.com>
2023-10-09 20:28:03 -0230

    comments

--- src/com/keyin/Library.java
+++ src/com/keyin/Library.java
@@ -2,54 +2,74 @@ package com.keyin;
 
 public class Library {
 
+    // Instance Variables
+
+    // 'books' is an array that holds Book objects, representing the collection of books in the library.
     private Book[] books;
-    public Library() {this.books = new Book[] {
-            new Book("The Chronicles of Narnia","CS lewis","234"),
-            new Book("The Lord of the rings","Tolkin","567" ),
-            new Book("Twilight","Unimportant","89"),
-            new Book("The catcher and the rye","History Guy","00" ),
-            new Book("Eragon","Chris","1" )
-    };
+
+    // Constructors
+
+    // This constructor initializes a new Library object with a predefined set of books.
+    public Library() {
+        this.books = new Book[] {
+                new Book("The Chronicles of Narnia","CS Lewis","234"),
+                new Book("The Lord of the Rings","Tolkien","567" ),
+                new Book("Twilight","Unimportant","89"),
+                new Book("The Catcher and the Rye","History Guy","00" ),
+                new Book("Eragon","Chris","1" )
+        };
     }
 
-    public Book[]searchBook(String query) {
-        int foundbook = 0;
+    // Methods
+
+    // This method searches for books in the library that match the provided query (in title, author, or ISBN)
+    // and returns an array of all matching Book objects.
+    public Book[] searchBook(String query) {
+        int foundBook = 0;
+        // Count the number of books that match the query.
         for (Book book : books){
-            if (book.getTitle().toLowerCase().contains(query.toLowerCase()) || book.getAuthor().toLowerCase().contains(query.toLowerCase()) || book.getIsbn().toLowerCase().contains(query.toLowerCase())) {
-                foundbook++;
+            if (book.getTitle().toLowerCase().contains(query.toLowerCase()) ||
+                    book.getAuthor().toLowerCase().contains(query.toLowerCase()) ||
+                    book.getIsbn().toLowerCase().contains(query.toLowerCase())) {
+                foundBook++;
             }
         }
 
+        // Create an array to store the matching books.
+        Book[] results = new Book[foundBook];
 
-        Book[] results = new Book[foundbook];
-
-
-       int resultindex = 0;
-
+        int resultIndex = 0;
+        // Add the matching books to the results array.
         for (Book book : books) {
-            if (book.getTitle().toLowerCase().contains(query.toLowerCase()) || book.getAuthor().toLowerCase().contains(query.toLowerCase()) || book.getIsbn().toLowerCase().contains(query.toLowerCase())) {
-                results[resultindex] = book;
-                resultindex++;
+            if (book.getTitle().toLowerCase().contains(query.toLowerCase()) ||
+                    book.getAuthor().toLowerCase().contains(query.toLowerCase()) ||
+                    book.getIsbn().toLowerCase().contains(query.toLowerCase())) {
+                results[resultIndex] = book;
+                resultIndex++;
             }
         }
 
         return results;
-
     }
 
+    // This method allows a book to be checked out of the library using its ISBN.
+    // It returns a message indicating the status of the checkout process.
     public String checkoutBook(String isbn) {
         for (Book book : books) {
-            if (book.getIsbn().equals(isbn));
-            if (book.getAvailability()) {
-                book.setAvailability(false); // Set the book as checked out
-                return "Book " + book.getTitle() + " checked out! Please return eventually.";
-            } else if (!book.getAvailability()) {
-                return "Book not available.";
+            if (book.getIsbn().equals(isbn)) {
+                if (book.getAvailability()) {
+                    book.setAvailability(false); // Set the book as checked out
+                    return "Book " + book.getTitle() + " checked out! Please return eventually.";
+                } else {
+                    return "Book not available.";
+                }
             }
         }
         return "Book not found.";
     }
 
+    // This method allows a book to be returned to the library using its ISBN.
+    // It returns a message indicating the status of the return process.
     public String returnBook(String isbn) {
         for (Book book : books) {
             if (book.getIsbn().equals(isbn)) {
@@ -62,7 +82,4 @@ public class Library {
         }
         return "Book not found.";
     }
-
-
-
 }
--- src/com/keyin/menu.java
+++ src/com/keyin/menu.java
@@ -1,29 +1,41 @@
 package com.keyin;
 import java.util.Scanner;
+
 public class menu {
 
+    // Instance Variables
+
+    // 'library' is a static instance of the Library class, used to interact with the library's functionalities.
     private static Library library = new Library();
 
+    // Main Method
+
+    // The entry point of the program. This method is executed when the program starts.
     public static void main(String[] args) {
+        // 'scanner' is used to read the user's input from the console.
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to my libary we hope you have a good time and kept quiet or else...");
+        // Welcome message displayed to the user.
+        System.out.println("Welcome to my library. We hope you have a good time and keep quiet or else...");
 
-
-
+        // Infinite loop to keep the menu running until the user chooses to exit.
         while (true) {
-            System.out.println("\nmenu");
+            // Displaying the menu options to the user.
+            System.out.println("\nMenu");
             System.out.println("1. Search for a book");
             System.out.println("2. Checkout a book");
             System.out.println("3. Return a book");
             System.out.println("4. Exit");
             System.out.println("\nEnter your choice");
 
+            // Reading the user's choice from the console.
             int choice = scanner.nextInt();
-            scanner.nextLine();
+            scanner.nextLine(); // Consume the newline character left by nextInt()
 
+            // Switch statement to handle the user's choice.
             switch (choice) {
                 case 1:
+                    // Search for a book.
                     System.out.print("Enter search query: ");
                     String query = scanner.nextLine();
                     Book[] results = library.searchBook(query);
@@ -37,26 +49,26 @@ public class menu {
                     }
                     break;
                 case 2:
+                    // Checkout a book.
                     System.out.print("Enter ISBN to checkout: ");
                     String checkoutIsbn = scanner.nextLine();
                     System.out.println(library.checkoutBook(checkoutIsbn));
                     break;
                 case 3:
+                    // Return a book.
                     System.out.print("Enter ISBN to return: ");
                     String returnIsbn = scanner.nextLine();
                     System.out.println(library.returnBook(returnIsbn));
                     break;
-
-
                 case 4:
+                    // Exit the program.
                     System.out.println("\nThank you for using the Library Catalog,");
-                    System.out.println("Who uses library's anymore nerd!");
-                    return;
+                    System.out.println("Who uses libraries anymore, nerd!");
+                    return; // Exit the program
                 default:
+                    // Handle invalid choices.
                     System.out.println("Invalid choice. Please try again.");
             }
-
         }
     }
-
 }
